14a94860b581ac875af068406420513183f91974
abdussamadmohd98 <87348847+abdussamadmohd98@users.noreply.github.com>
2021-07-13 10:11:36 +0530

    Add files via upload

---
+++ src/typecasting.java
@@ -0,0 +1,20 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte b = 20;
+int i = b;
+byte d = (byte)i;
+
+byte e = 20;
+byte f = 6;
+int g = e*f;
+System.out.println("the result is:" +g);
+int x=15;
+int y=10;
+byte z=(byte)(x*y);
+System.out.println(z);
+	}
+
+}
